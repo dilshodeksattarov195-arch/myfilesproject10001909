dc05d7e2df833b333ad7a279c49ae8fae7e04be6
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 07:57:21 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class validatorFonnectWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = validatorFonnectWorker()
+    print("Worker engine initialized.")
